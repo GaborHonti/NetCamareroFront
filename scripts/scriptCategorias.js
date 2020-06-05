b99a873a8f20303af67241d9016f039513606fbf
unknown <ghonti67@gmail.com>
2020-06-05 16:07:38 +0200

    cambiada portada

--- scripts/scriptCategorias.js
+++ scripts/scriptCategorias.js
@@ -33,7 +33,7 @@ var app = new Vue({
             //guardamos los valores para poder trabajarlos posteriormente
             localStorage.setItem("crit", 'categoria');
             localStorage.setItem("val", category);
-            location.replace('pages/busca.html');
+            location.replace('busca.html');
         },
     },
    
