5e1a530805c2af9a28c1b53cb3351d77883351d2
unknown <ghonti67@gmail.com>
2020-06-05 15:52:41 +0200

    cambiada portada

---
+++ scripts/scriptLocalidades.js
@@ -0,0 +1,32 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+      message: 'Hello Vue!',
+      logged: 0,
+    },
+    created(){
+        this.getRestaurantes(1);
+        token = localStorage.getItem("token");
+        if(token != null){
+           this.logged = 1; //esta logged el user
+        }
+    },
+    /*mounted () {
+        axios
+          .get('api/restaurants')
+          .then(response => {(this.message = response.data.restaurantes.data),
+        this.pagination = response.data.pagination})
+    },*/
+    methods: {
+        getRestaurantes: function(page){
+            axios
+            .get(localStorage.getItem('URL_API') + 'cities')
+            .then(response => {(this.message = response.data)})
+        },
+        salir: function(){
+            localStorage.removeItem("token");
+            location.reload();
+        }
+    },
+   
+})
